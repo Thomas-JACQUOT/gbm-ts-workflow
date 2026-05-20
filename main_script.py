df0a51c6c5170b926e9e682a110b0e8499131648
jacquot <jacquot@>
2026-05-20 13:50:31 +0200

    COrrecting inheritage of jobs

--- main_script.py
+++ main_script.py
@@ -167,8 +167,8 @@ def main():
                 submit_dag_file.write("VARS TS" + str(i) + ''' line="''' + str(fermi_time_array[i]) + '''" dir=" ''' + os.path.join(output_path, str(time_list[int(i/1440)*1440]).split('T')[0], str(time_list[i]).split('T')[1][0:2]) + '''" output=" ''' + os.path.join(output_path, str(time_list[int(i/1440)*1440]).split('T')[0], "output_TS",str(time_list[i]).split('T')[1].replace(":", "/")) + '''"\n''')
         for i in range(len(fermi_time_array)):
                 submit_dag_file.write("PARENT")
-                for j in range(int(len(time_list)/60)):
-                        submit_dag_file.write(" A" + str(int(j/24)) + " B"+ str(j))	
+                for j in range(int(len(time_list)/1440)):
+                        submit_dag_file.write(" A" + str(int(j)) + " B"+ str(j))	
                 submit_dag_file.write(" CHILD TS" + str(i) + "\n")
         for i in range(len(fermi_time_array)):
                 submit_dag_file.write("PARENT TS" + str(i) + " CHILD C\n")
